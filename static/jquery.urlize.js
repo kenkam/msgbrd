e68eeaec3952db6aac74c559e9887d3ea980b0cf
Kenneth Kam <kenkam@gmail.com>
2011-10-29 22:58:55 +0100

    Fixes urlize bug and adds a snippet on index.html

--- static/jquery.urlize.js
+++ static/jquery.urlize.js
@@ -1,6 +1,6 @@
 jQuery.fn.urlize = function( base ) {
 	var x = this.html();
-	list = x.match( /\b(http:\/\/|www\.|http:\/\/www\.)[^ <>]{2,100}\b/g );
+	list = x.match( /\b(http:\/\/|www\.|https:\/\/www\.)[^ <>]{2,100}\b/g );
 	if ( list ) {
 		for ( i = 0; i < list.length; i++ ) {
 		    x = x.replace( list[i], "<a href='" + list[i] + "'>"+ list[i] + "</a>" );
